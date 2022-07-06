a3f365ea0e9036a42ab87ea899dde8bd2e9bc044
euerickap <erick.almeida86@hotmail.com>
2022-07-05 22:46:17 -0300

    creating a server and db

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const server = express()
 
+// pegar o banco de dados
+const db = require("./database/db")
+
 // configurar pasta pública
 server.use(express.static("public"))
 
@@ -24,6 +27,17 @@ server.get("/create-point", (req, res) => {
 })
 
 server.get("/search", (req, res) => {
+
+  // pegar os dados do banco de dados
+  db.all(`SELECT * FROM places`, function(err, rows) {
+    if(err) {
+      return console.log(err)
+    }
+  
+      console.log("Aqui estão seus registros: ")
+      console.log(rows)
+  })
+
   return res.render("search-results.html")
 })
 
